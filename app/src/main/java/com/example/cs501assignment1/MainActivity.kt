3a958445434050c95cf6831dbf413da522135f17
enano1 <paul.m.enano@gmail.com>
2024-01-19 01:07:44 -0500

    added support to main

--- app/src/main/java/com/example/cs501assignment1/MainActivity.kt
+++ app/src/main/java/com/example/cs501assignment1/MainActivity.kt
@@ -2,9 +2,8 @@ package com.example.cs501assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.TextView
-import android.widget.ToggleButton
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -12,31 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toggleButton = findViewById<ToggleButton>(R.id.toggleButton)
+        val toggleButton = findViewById<Button>(R.id.toggleButton)
         val textViewButton = findViewById<TextView>(R.id.textViewButton)
-
         val toolbar =  findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        toggleButton.setOnCheckedChangeListener {_, isClicked ->
-            if (isClicked) {
-                textViewButton.visibility = View.VISIBLE
-            } else {
-                textViewButton.visibility = View.INVISIBLE
-            }
+        toggleButton.setOnClickListener{
+            textViewButton.text = "Hello World"
         }
     }
 }
 
-// resources used for this assignment in XML:
-// https://stackoverflow.com/questions/49402001/how-to-set-visibility-in-kotlin
-// https://www.geeksforgeeks.org/togglebutton-in-kotlin
-// ^^ this taught me toggleButton.setOnCheckedChangeListener... in main
+// Resources used for this assignment in XML:
+// https://www.geeksforgeeks.org/button-in-kotlin/
+// ^^ this taught me toggleButton.setOnClickListener... in main
 // ^^ this taught me  android:id="@+/..." to refer to variables from main
 // https://www.geeksforgeeks.org/toolbar-in-android-with-example/
 // ^^ this taught me how to create a toolbar with text
 // ^^ also taught me how to change the color of toolbar text
-// https://stackoverflow.com/questions/4127725/how-can-i-remove-a-button-or-make-it-invisible-in-android
 
 
 
